ad3493eab2e385d7697ac83e9a2d7ded8561ec8a
dpisati <43629737+dpisati@users.noreply.github.com>
2021-11-19 20:53:06 +1300

    stripe webhooks completed

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream'
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
+
 
 async function buffer(readable: Readable) {
     const chunks =[];
@@ -21,11 +23,12 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-
     if(req.method === "POST") {
         const buf = await buffer(req);
         const secret = req.headers['stripe-signature']
@@ -41,7 +44,40 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const { type } = event;
 
         if(relevantEvents.has(type)) {
-            console.log("Evento recebido", event);
+            try {
+                switch(type) {
+                    case 'checkout.session.completed':
+                        console.log('Checkout session completed', type)
+
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString(),
+                            true
+                        )
+                        break;
+
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+
+                        console.log('subscription completed', type)
+
+                            const subscription = event.data.object as Stripe.Subscription
+
+                            await saveSubscription(
+                                subscription.id,
+                                subscription.customer.toString(),
+                                false
+                            )
+                        break;
+
+                    default:
+                        throw new Error('Unhandled event.')
+                }
+            } catch (error) {
+                return res.json({ error: 'Weebhook handler failed'})
+            }
         }
     
         res.status(200).json({ received: true});
