977b7b0cd373e94d252980c876acc78cbb75b011
dpisati <43629737+dpisati@users.noreply.github.com>
2021-11-21 17:52:24 +1300

    working with CMS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import SignInButton from '../SignInButton';
+import Link from 'next/Link';
 
 import styles from './styles.module.scss';
+import { useRouter } from 'next/dist/client/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+
+    
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="dp.news" />
                 <nav>
-                    <a className={styles.active} href="#">Home</a>
-                    <a href="#">Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>                    
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>            
