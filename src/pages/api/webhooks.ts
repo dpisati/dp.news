556721f6833878b46d782d6e3cff28fd81573a69
dpisati <43629737+dpisati@users.noreply.github.com>
2022-05-23 09:46:51 +1200

    logs

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,17 +1,14 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { Readable } from 'stream'
-import Stripe from "stripe";
-import { stripe } from "../../services/stripe";
-import { saveSubscription } from "./_lib/manageSubscription";
-
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
-    const chunks =[];
+    const chunks = [];
 
     for await (const chunk of readable) {
-        chunks.push(
-            typeof chunk === "string" ? Buffer.from(chunk) : chunk
-        );
+        chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
     }
     return Buffer.concat(chunks);
 }
@@ -19,70 +16,81 @@ async function buffer(readable: Readable) {
 export const config = {
     api: {
         bodyParser: false,
-    }
-}
+    },
+};
 
 const relevantEvents = new Set([
     'checkout.session.completed',
     'customer.subscription.updated',
     'customer.subscription.deleted',
-])
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method === "POST") {
+    if (req.method === 'POST') {
         const buf = await buffer(req);
-        const secret = req.headers['stripe-signature']
+        const secret = req.headers['stripe-signature'];
 
         let event: Stripe.Event;
 
         try {
-            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+            console.log('buf', buf);
+            console.log('secret', secret);
+            console.log(
+                'process.env.STRIPE_WEBHOOK_SECRET',
+                process.env.STRIPE_WEBHOOK_SECRET
+            );
+            event = stripe.webhooks.constructEvent(
+                buf,
+                secret,
+                process.env.STRIPE_WEBHOOK_SECRET
+            );
         } catch (e) {
             return res.status(400).send(`Webhook error: ${e.message}`);
         }
 
         const { type } = event;
 
-        if(relevantEvents.has(type)) {
+        if (relevantEvents.has(type)) {
             try {
-                switch(type) {
+                switch (type) {
                     case 'checkout.session.completed':
-                        console.log('Checkout session completed', type)
+                        console.log('Checkout session completed', type);
 
-                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+                        const checkoutSession = event.data
+                            .object as Stripe.Checkout.Session;
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
                             checkoutSession.customer.toString(),
                             true
-                        )
+                        );
                         break;
 
                     case 'customer.subscription.updated':
                     case 'customer.subscription.deleted':
+                        console.log('subscription completed', type);
 
-                        console.log('subscription completed', type)
+                        const subscription = event.data
+                            .object as Stripe.Subscription;
 
-                            const subscription = event.data.object as Stripe.Subscription
-
-                            await saveSubscription(
-                                subscription.id,
-                                subscription.customer.toString(),
-                                false
-                            )
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false
+                        );
                         break;
 
                     default:
-                        throw new Error('Unhandled event.')
+                        throw new Error('Unhandled event.');
                 }
             } catch (error) {
-                return res.json({ error: 'Weebhook handler failed'})
+                return res.json({ error: 'Weebhook handler failed' });
             }
         }
-    
-        res.status(200).json({ received: true});
+
+        res.status(200).json({ received: true });
     } else {
         res.setHeader('Allow', 'POST');
         res.status(405).end('Method not allowed');
     }
-}
+};
